95fd302c22f63e76274f95b94c171a69ead42289
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-07-12 21:26:48 -0400

    PClienteView for viewing client's info was set up

---
+++ src/views/PClienteView.java
@@ -0,0 +1,23 @@
+package views;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+import logic.Utilidad;
+
+public class PClienteView extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public PClienteView() {
+		setPreferredSize(
+				new Dimension(
+						Utilidad.witdhForParentPanelView,
+						Utilidad.heightForParentPanelView
+				)
+		);
+	}
+
+}
